fdd0bc184133194cc2c2a3cffa42745332683a0e
juergen.windhaber <juergen.windhaber@gmail.com>
2019-06-26 22:34:24 +0200

    one second beep tone for the hangboard timer routine

--- app/src/main/java/com/example/hangboard/timer/HangboardTimer.kt
+++ app/src/main/java/com/example/hangboard/timer/HangboardTimer.kt
@@ -2,8 +2,8 @@ package com.example.hangboard.timer
 
 import android.media.AudioManager
 import android.media.ToneGenerator
+import android.os.Handler
 import android.os.SystemClock
-import android.util.Log
 import com.example.hangboard.timeline.Timeline
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -11,30 +11,58 @@ import kotlinx.coroutines.channels.ticker
 import kotlinx.coroutines.launch
 
 
-class HangboardTimer(private val timeline: Timeline, private val onUpdateCallback: (secondsLeft: Int, exerciseText: String, color: String) -> Unit) {
+class HangboardTimer(
+    private val timeline: Timeline,
+    private val onUpdateCallback: (secondsLeft: Int, exerciseText: String, color: String) -> Unit
+) {
 
-    val toneGen1 = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+
+    val toneGenerator = ToneGenerator(AudioManager.STREAM_ALARM, 100)
 
     fun startTimer() {
 
         val tickerChannel = ticker(delayMillis = 1000, initialDelayMillis = 0)
 
-        val elapsedRealtime = SystemClock.elapsedRealtime()
-
-        GlobalScope.launch(Dispatchers.Main){
-            timeline.timeLineFragments.forEach {fragment ->
+        GlobalScope.launch(Dispatchers.Main) {
+            timeline.timeLineFragments.forEachIndexed { index, fragment ->
                 var secondsLeft = fragment.durationInSeconds
-               // toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
-                val elapsedRealtime = SystemClock.elapsedRealtime()
-                toneGen1.startTone(ToneGenerator.TONE_PROP_BEEP2)
-                Log.i("sdfs", "Millis: " + (SystemClock.elapsedRealtime() - elapsedRealtime))
+
+                // lets skip the first one
+                if (index != 0) {
+                    soundOnEndHold()
+                }
+
                 repeat(fragment.durationInSeconds) {
                     tickerChannel.receive()
-                    onUpdateCallback(secondsLeft, fragment.fragmentIdentifier.toString(), fragment.fragmentIdentifier.color)
+                    onUpdateCallback(
+                        secondsLeft,
+                        fragment.fragmentIdentifier.toString(),
+                        fragment.fragmentIdentifier.color
+                    )
                     secondsLeft--
                 }
             }
             tickerChannel.cancel()
         }
+    }
+
+
+    fun soundOnEndHold() {
+
+        val handler = Handler()
+        val timeNow = SystemClock.uptimeMillis()
+
+        handler.postAtTime({
+            toneGenerator.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 375)
+        }, 0 + timeNow)
+
+        handler.postAtTime({
+            toneGenerator.startTone(ToneGenerator.TONE_CDMA_KEYPAD_VOLUME_KEY_LITE, 120)
+        }, 880 + timeNow)
+
+//        ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, "TONE_CDMA_ALERT_CALL_GUARD") // Perfect double
+//        ToneGenerator.TONE_CDMA_KEYPAD_VOLUME_KEY_LITE, "TONE_CDMA_KEYPAD_VOLUME_KEY_LITE") //perfect one timer
+//        ToneGenerator.TONE_CDMA_PIP, "TONE_CDMA_PIP")
+
     }
 }
