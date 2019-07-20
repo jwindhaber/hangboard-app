28f677238b85eaf91b5dd98793703b6c1588b382
juergen.windhaber <juergen.windhaber@gmail.com>
2019-07-20 20:24:00 +0200

    preview refined

--- app/src/main/java/com/example/hangboard/timer/HangboardTimer.kt
+++ app/src/main/java/com/example/hangboard/timer/HangboardTimer.kt
@@ -22,12 +22,10 @@ class HangboardTimer(private val workout: Workout) {
 
         val workoutTimeline = mutableListOf<WorkoutTimelineUnit>()
 
-
         //Initial Pause
-
-        val initialActivityName = "${workout.activities.first().name}: (0/${workout.activities.first().exercises.size})"
+        val initialActivityName = "${workout.activities.first().name}: ${workout.activities.first().exercises.size}"
         val initialWeight = workout.activities.first().exercises.first().weight
-        val initialReps = "0/${workout.activities.first().exercises.first().repetitions}"
+        val initialReps = "${workout.activities.first().exercises.first().repetitions}"
         val initialRest = 10
         val initialOverallSecondsLeft = initialRest + HanboardTimerUtils.evaluateRemainingTime(workout.activities, 0, 0)
         workoutTimeline.add(WorkoutTimelineUnit(initialActivityName, initialWeight, initialReps, initialOverallSecondsLeft, SESSION_REST, initialRest))
@@ -35,7 +33,7 @@ class HangboardTimer(private val workout: Workout) {
         workout.activities.forEachIndexed { activitiesIndex, activity ->
 
             activity.exercises.forEachIndexed { exerciseIndex, exercise ->
-                val activityName = "${activity.name}: (${exerciseIndex + 1}/${activity.exercises.size})"
+                val activityName = "${activity.name}: ${exerciseIndex + 1}/${activity.exercises.size}"
                 var overallSecondsLeft = HanboardTimerUtils.evaluateRemainingTime(workout.activities, activitiesIndex, exerciseIndex)
                 val secondsWorkUnitWork = exercise.workUnit.work
                 val secondsWorkUnitRest = exercise.workUnit.rest
@@ -53,23 +51,26 @@ class HangboardTimer(private val workout: Workout) {
 
                 var nextActivityName = ""
                 var nextWeight = 0
+                var nextReps = ""
                 when {
                     exerciseIndex + 1 < activity.exercises.size -> {
-                        nextActivityName = "NEXT: ${activity.name}: (${exerciseIndex + 2}/${activity.exercises.size})"
+                        nextActivityName = "${activity.name}: ${exerciseIndex + 2}/${activity.exercises.size}"
                         nextWeight = activity.exercises[exerciseIndex + 1].weight
+                        nextReps = "${activity.exercises[exerciseIndex + 1].repetitions}"
                     }
                     activitiesIndex + 1 < workout.activities.size -> {
-                        nextActivityName = "NEXT: ${workout.activities[activitiesIndex + 1].name}"
+                        nextActivityName = "${workout.activities[activitiesIndex + 1].name}: ${workout.activities[activitiesIndex + 1].exercises.size}"
                         nextWeight = workout.activities[activitiesIndex + 1].exercises.first().weight
+                        nextReps = "${workout.activities[activitiesIndex + 1].exercises.first().repetitions}"
                     }
                     else -> {
                         nextActivityName = "GAME OVER"
                         nextWeight = 0
+                        nextReps = "-"
                     }
                 }
 
-
-                workoutTimeline.add(WorkoutTimelineUnit(nextActivityName, nextWeight, "(-/-)", overallSecondsLeft, SESSION_REST, exercise.rest))
+                workoutTimeline.add(WorkoutTimelineUnit(nextActivityName, nextWeight, nextReps, overallSecondsLeft, SESSION_REST, exercise.rest))
 
             }
 
--- app/src/main/java/com/example/hangboard/workout/definition/FragmentIdentifier.kt
+++ app/src/main/java/com/example/hangboard/workout/definition/FragmentIdentifier.kt
@@ -1,7 +1,7 @@
 package com.example.hangboard.workout.definition
 
 enum class FragmentIdentifier(val text: String, val color: String) {
-    REST("REST","#de935f"),
+    REST("REST","#f0c674"),
     WORK("WORK","#ab4642"),
     SESSION_REST("REST","#a1b56c")
 }
